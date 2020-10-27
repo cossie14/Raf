d68656addc9b546686bbf2a83d6f6eb9a93ee8ef
Sylviah Rutto <sylviah.rutt0@gmail.com>
2020-10-27 15:56:23 +0300

    all

--- myapi/urls.py
+++ myapi/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from api import views
 from rest_framework.schemas import get_schema_view
-
+from rest_framework.authtoken.views import obtain_auth_token 
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -16,6 +16,7 @@ urlpatterns = [
    
     path('schema/', schema_view),
     path('', views.api_root),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
      
